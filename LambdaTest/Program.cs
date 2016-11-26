ea5cfff2fec5230fa5d178c3782d93b57ad63959
deo <patrick@caddeo.dk>
2016-11-26 13:16:35 +0100

    Before initial rework for multiply search parameters

--- LambdaTest/Program.cs
+++ LambdaTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,18 @@ namespace LambdaTest
     class Program
     {
         static void Main(string[] args)
+        {
+            // The search algorithm could be improved a lot
+            // but it was more to try out funcs and lambdas in methods
+            // also i am not sure why i am calling the expressions 'wildcards'... 
+            Program program = new Program();
+            program.Run();
+
+            // TODO: Only allow left hand expressions for now 
+            // TODO: If it's a right hand expression, flip the wildcard
+        }
+
+        public void Run()
         {
             var repository = new PersonRepository(new Person("Patrick", new DateTime(1993, 11, 23)), new Person("Tobias", new DateTime(2004, 12, 1)));
 
@@ -23,13 +36,13 @@ namespace LambdaTest
             {
                 input = Console.ReadLine();
 
-                if(input.ToLower().Equals("clear"))
+                if (input.ToLower().Equals("clear"))
                 {
                     Console.Clear();
                     continue;
                 }
 
-                if(string.IsNullOrEmpty(input))
+                if (string.IsNullOrEmpty(input))
                 {
                     continue;
                 }
@@ -44,17 +57,17 @@ namespace LambdaTest
 
                 var persons = GetPersonsFromRepository(query.Item2, query.Item1, repository);
 
-                if (persons == null)
+                if (persons == null || persons.Count == 0)
                 {
                     Console.WriteLine("Found 0");
                     continue;
                 }
 
-                Console.WriteLine($"Found ({persons.Count}):");
+                Console.WriteLine($"Found ({persons.Count}) (age {query.Item2} {query.Item1}):");
 
                 foreach (Person person in persons)
                 {
-                    var text = $"Person: {person.Name}, Birthday: {person.Birthday.Value.ToString("dd-MM-yyyy")}";
+                    var text = $"Person: {person.Name}, Birthday: {person.Birthday.Value.ToString("dd-MM-yyyy")}, Age: {person.Age}";
                     Console.WriteLine(text);
                 }
             } while (input != "q");
@@ -65,19 +78,28 @@ namespace LambdaTest
         }
 
         // This method should be split up
-        public static Tuple<string, char> GetSearchQuery(string input)
+        public Tuple<string, char> GetSearchQuery(string input)
         {
             if(string.IsNullOrEmpty(input))
             {
                 return null;
             }
 
+            // TODO: Split search query up in groups
+            // i.e. age > 10 && age < 30 
+            // or age > 10 || birthday = 23-11-93 
+
             var inputSplit = input.Split(' ');
 
-            char[] wildCards = { '>', '<', '=' };
+            // See if the first is a string, get the string group then (i.e 'age') 
+            // then check if the next is a searchParameter
+            // then check if the third is a value
+
+            char[] operators = { '>', '<', '=' };
+
 
             // index of wildcard and wildcard
-            var wildcardsFound = new Dictionary<int, char>();
+            var searchOperatorsFound = new Dictionary<int, char>();
 
             for(int i=0; i<inputSplit.Length; i++)
             {
@@ -88,51 +110,54 @@ namespace LambdaTest
                     continue;
                 }
 
-                var wildcard = ' ';
-                char.TryParse(current, out wildcard);
+                var searchOperator = ' ';
+                char.TryParse(current, out searchOperator);
 
-                if(wildCards.Contains(wildcard))
+                if(operators.Contains(searchOperator))
                 {
-                    wildcardsFound.Add(i, wildcard);
+                    searchOperatorsFound.Add(i, searchOperator);
                 }
             }
 
-            if(wildcardsFound.Count == 0)
+            if(searchOperatorsFound.Count == 0)
             {
                 return null;
-            };
+            }
 
-            var firstWildcard = wildcardsFound.First();
+            var firstOperator = searchOperatorsFound.First();
 
-            if(inputSplit.Length-1 >= firstWildcard.Key + 1)
+            if(inputSplit.Length-1 >= firstOperator.Key + 1)
             {
-                var searchParamter = inputSplit[firstWildcard.Key + 1];
-                var wildcardSearch = firstWildcard.Value;
+                var searchParamter = inputSplit[firstOperator.Key + 1];
+                var operatorSearch = firstOperator.Value;
 
-                return new Tuple<string, char>(searchParamter, wildcardSearch);
+                return new Tuple<string, char>(searchParamter, operatorSearch);
             }
             else if(inputSplit.Length-1 > 0)
             {
-                var searchParamter = inputSplit[firstWildcard.Key - 1];
-                var wildcardSearch = firstWildcard.Value;
+                var searchParamter = inputSplit[firstOperator.Key - 1];
+                var operatorSearch = firstOperator.Value;
 
-                return new Tuple<string, char>(searchParamter, wildcardSearch);
+                return new Tuple<string, char>(searchParamter, operatorSearch);
             }
 
             return null;
         }
 
-        public static List<Person> GetPersonsFromRepository(char wildcard, string search, PersonRepository repo)
+        public List<Person> GetPersonsFromRepository(char searchOperator, string search, PersonRepository repo)
         {
-            var ageSearch = -1;
-            int.TryParse(search, out ageSearch);
+            var ageSearch = 0;
 
-            if(ageSearch < 0)
+            bool result = int.TryParse(search, out ageSearch);
+
+            if(result == false || ageSearch < 0)
             {
                 return null;
             }
 
-            switch(wildcard)
+            Expression<Func<Person, bool>> lambda = x => x.Age > ageSearch;
+
+            switch(searchOperator)
             {
                 case '>':
                     return repo.GetPersons(x => x.Age > ageSearch);
@@ -179,14 +204,19 @@ namespace LambdaTest
     {
         public static List<Person> Persons { get; protected set; }
 
-        public PersonRepository(params Person[] persons)
+        public PersonRepository(List<Person> persons)
         {
             Persons = new List<Person>();
 
-            foreach(Person person in persons)
+            foreach (Person person in persons)
             {
                 this.AddPerson(person);
             }
+        }
+
+        public PersonRepository(params Person[] persons) : this(persons.ToList())
+        {
+
         }
 
         public void AddPerson(Person person)
@@ -210,11 +240,6 @@ namespace LambdaTest
         public Person GetPerson(Func<Person, bool> query)
         {
             return GetPersons(query).FirstOrDefault();
-        }
-
-        public void SetList(List<Person> persons)
-        {
-            Persons = persons;
         }
     }
 }
